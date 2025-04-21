cd2e3a6555521255394476f90fdc89ab0722e61c
Vinícius Dias <vyvy292004@gmail.com>
2025-04-21 15:33:52 -0300

    feat(groceries_screen): adds initial dummy data to the groceries screen

--- lib/screens/groceries_screen.dart
+++ lib/screens/groceries_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/widgets/grocery.dart';
 
 class GroceriesScreen extends StatelessWidget {
   const GroceriesScreen({super.key});
@@ -6,7 +8,12 @@ class GroceriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Your groceries')),
-      body: Column(),
+      body: Column(
+        children:
+            groceryItems
+                .map((groceryItem) => Grocery(groceryItem: groceryItem))
+                .toList(),
+      ),
     );
   }
 }
---
+++ lib/widgets/grocery.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+
+class Grocery extends StatelessWidget {
+  const Grocery({super.key, required this.groceryItem});
+  final GroceryItem groceryItem;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 20,
+        height: 20,
+        color: groceryItem.category.color,
+      ),
+      title: Text(groceryItem.name),
+      trailing: Text(groceryItem.quantity.toString()),
+    );
+  }
+}
